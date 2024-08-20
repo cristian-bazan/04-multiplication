9992ebd76cf5142ba2d9369bffc8b6e74e34a06b
404937-Bazan Cristian <cristian.bazan@yahoo.com>
2024-08-20 15:17:12 -0300

    first commit

---
+++ test/config/plugins/args.plugin.test.ts
@@ -0,0 +1,48 @@
+import {beforeEach} from "node:test";
+
+const runCommand = async (args: string[]) => {
+
+    process.argv = [...process.argv, ...args];
+
+    const {yarg} = await import("../../../src/config/plugins/args.plugin");
+
+    return yarg;
+}
+
+describe('Test args.plugin.ts', () => {
+
+    const originalArgv = process.argv;
+
+    beforeEach(() =>{
+        process.argv = originalArgv;
+        jest.resetModules();
+    });
+
+    test('should return default values', async ()=>{
+
+        const argv = await runCommand(['-b', '5']);
+
+       expect(argv).toEqual( expect.objectContaining({
+
+           b:5,
+           l:10,
+           s:false,
+           n:'multiplication-table',
+           d:'outputs',
+       }));
+    });
+
+    test('should return configuration with custom values', async()=>{
+
+        const argv = await runCommand(['-b', '7', '-l', '5', '-s', '-n', 'table', '-d', 'salida']);
+
+        expect(argv).toEqual( expect.objectContaining({
+
+            b:7,
+            l:5,
+            s:true,
+            n:'table',
+            d:'salida',
+        }));
+    });
+});
